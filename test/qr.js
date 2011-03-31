d6cd4c2b7bd1ad8a059d2712dd1646130af38fd1
Brian Celenza <bcelenza@gmail.com>
2011-03-31 15:11:01 -0400

    added stderr listener

--- test/qr.js
+++ test/qr.js
@@ -107,7 +107,7 @@ vows.describe('Encoder').addBatch({
                 },
                 'emits an \'error\' event with Error': function(result, encoder) {
                     assert.ok((result instanceof Error));
-                    assert.notEqual(result.message, null);
+                    assert.notEqual(result.message, "");
                 }
             }
         }
