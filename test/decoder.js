05d4a82f60fe51e206e8704061890324d4a28266
Brian Celenza <bcelenza@gmail.com>
2011-04-08 15:42:22 -0400

    unit test is now properly searching for the java command

--- test/decoder.js
+++ test/decoder.js
@@ -12,8 +12,15 @@ var test_file_name = './test-decoder.png';
 vows.describe('Decoder').addBatch({
     'The decoder library': {
         topic: function() {
-            var which = child_process.spawn('which', ['qrencode']);
-            which.on('exit', this.callback);
+            var cmd_options = {
+                cwd: __dirname + '/../include/zxing-1.6'
+            };
+            var java = child_process.spawn('java', [
+                '-cp', 
+                'javase/javase.jar:core/core.jar', 
+                'com.google.zxing.client.j2se.CommandLineRunner'
+            ], cmd_options);
+            java.on('exit', this.callback);
         },
         'is installed and available': function(exit_code, process) {
             assert.equal(exit_code, null, 'the decoder library does not appear to have been installed properly');
